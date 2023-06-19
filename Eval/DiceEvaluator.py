2949afcd0effc6faa735fe5b4f9ea0834d5a331e
YunF8967 <yunfang2222@gmail.com>
2023-06-20 06:13:55 +1000

    git account test

--- Eval/DiceEvaluator.py
+++ Eval/DiceEvaluator.py
@@ -1,4 +1,14 @@
+from detectron2.evaluation import DatasetEvaluator
 
+class DiceEvaluator(DatasetEvaluator):
+    
+    def reset(self):
+        self.count = 0
 
-class DiceEvaluator():
-    pass
+    def process(self, inputs, outputs):
+        for output in outputs:
+            self.count += len(output["instances"])
+
+    def evaluate(self):
+        # save self.count somewhere, or print it, or return it.
+        return {"count": self.count}
